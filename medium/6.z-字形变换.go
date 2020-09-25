27df836ac51b2db04638e84aa96066b667dd6978
cocowh <cocowh@qq.com>
2020-09-25 20:04:27 +0800

    Z 字形变换

---
+++ medium/6.z-字形变换.go
@@ -0,0 +1,30 @@
+package medium
+
+/*
+ * @lc app=leetcode.cn id=6 lang=golang
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	length := len(s)
+	gap := 2*numRows - 2
+	var ret string
+	for i := 0; i < numRows; i++ {
+		for j := 0; j+i < length; j += gap {
+			ret += string(s[i+j])
+			if i != 0 && i != numRows-1 && j+gap-i < length {
+				ret += string(s[j+gap-i])
+			}
+		}
+	}
+	return ret
+}
+
+// @lc code=end
+
+// 想到的遍历字符串确定位置，然后拼接，或者按确定位置在字符串遍历然后拼接。
