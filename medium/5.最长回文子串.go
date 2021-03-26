f3aeb546ac36aa6d9c71ba82463c60f2ad5cc9fd
cocowh <cocowh@qq.com>
2021-03-26 18:03:51 +0800

    最长回文子串

--- medium/5.最长回文子串.go
+++ medium/5.最长回文子串.go
@@ -20,20 +20,20 @@ func longestPalindrome(s string) string {
 
 	start, maxLen := 0, 1
 	for i := 0; i < length; {
-		if length-i <= maxLen/2 {
+		if length-i <= maxLen/2 { //剩余的可搜索字串长度小于已寻找的最大长度，无寻找必要
 			break
 		}
 		b, e := i, i
-		for e < length-1 && s[e+1] == s[e] {
+		for e < length-1 && s[e+1] == s[e] { //多重复字符中心时，向右寻找中心边界
 			e++
 		}
-		i = e + 1
-		for e < length-1 && b > 0 && s[e+1] == s[b-1] {
+		i = e + 1                                       //下次搜索中心（跳过连续重复）
+		for e < length-1 && b > 0 && s[e+1] == s[b-1] { //双边拓展，e=b：单中心，e!=b：多字符中心
 			e++
 			b--
 		}
 		newLen := e + 1 - b
-		if newLen > maxLen {
+		if newLen > maxLen { //此次搜索回文子串是否为最大回文子串
 			start = b
 			maxLen = newLen
 		}
