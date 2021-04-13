62350e31fe9630fd9361a0ee218fa6023900469c
cocowh <cocowh@qq.com>
2021-04-13 17:26:42 +0800

    解码方法

--- medium/91.解码方法.go
+++ medium/91.解码方法.go
@@ -17,20 +17,21 @@ func numDecodings(s string) int {
 	if length == 0 || s[0] == '0' || (length > 1 && s[length-1] == '0' && s[length-2] > '2') {
 		return 0
 	}
-	pre, res := 1,1
-	for i := length-2; i > 0; i-- {
-		if s[i] == '0' { 
-			if s[i-1] == '0' || s[i-1] > '2' { // 中间连续两个0或者大于20，无法编码
+	pre, res := 1, 1
+	for i := 1; i < length; i++ {
+		temp := res
+		if s[i] == '0' {
+			if s[i-1] == '1' || s[i-1] == '2' { // 为0时只有s[i-1]为1或者2时才能编码
+				res = pre
+			} else {
 				return 0
 			}
-			res += 
-		} else if s[i] == '0' && s[]
-		if s[i] == '0' && s[i-1] > '2' {
-			return 
-		} else if s[i-1] <  {
-			
+		} else if s[i-1] == '1' || (s[i-1] == '2' && s[i] >= '1' && s[i] <= '6') { // 只有连续两个字符在01-26之间才可编码
+			res += pre
 		}
+		pre = temp
 	}
+	return res
 }
 
 // @lc code=end
