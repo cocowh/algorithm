50d6332a59f2d8e4c92f2aee474ece78fc2e03b0
cocowh <cocowh@qq.com>
2021-09-22 05:30:05 +0800

    合并两个排序的链表

---
+++ easy/offer.25.合并两个排序的链表.go
@@ -0,0 +1,45 @@
+package easy
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
+
+func mergeTwoListsII(l1 *ListNode, l2 *ListNode) *ListNode {
+	l := &ListNode{Next: l1}
+	curr := l
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			curr.Next = l1
+			l1 = l1.Next
+		} else {
+			curr.Next = l2
+			l2 = l2.Next
+		}
+		curr = curr.Next
+	}
+	if l1 == nil {
+		curr.Next = l2
+	} else {
+		curr.Next = l1
+	}
+	return l.Next
+}
