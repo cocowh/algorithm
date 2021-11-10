d6481139721aa375966eb7e6fa5f745a18ae4ae3
cocowh <cocowh@qq.com>
2021-11-11 04:29:58 +0800

    无重复字符的最长子串

--- medium/3.无重复字符的最长子串.go
+++ medium/3.无重复字符的最长子串.go
@@ -23,10 +23,13 @@ func lengthOfLongestSubstring(s string) int {
 	length, maxLen := len(s), 0
 	pMap := make(map[byte]int, length)
 	for i, j := 0, 0; j < length; j++ {
-		if _, ok := pMap[s[j]]; ok {
-			i = max(i, pMap[s[j]])
+		// 重复字符上一次出现的位置
+		if v, ok := pMap[s[j]]; ok {
+			i = max(i, v)
 		}
+		// 最长长度
 		maxLen = max(maxLen, (j - i + 1))
+		// 记录字符出现的位置
 		pMap[s[j]] = j + 1
 	}
 	return maxLen
