1656643b3f1e646b69cfd461cde729523fb47565
cocowh <cocowh@qq.com>
2021-11-15 05:21:14 +0800

    路径总和 II

--- medium/113.路径总和-ii.go
+++ medium/113.路径总和-ii.go
@@ -21,21 +21,21 @@ func PathSum(root *TreeNode, targetSum int) [][]int {
  */
 func pathSum(root *TreeNode, targetSum int) [][]int {
 	res := [][]int{}
-	pathSumHelper(root, targetSum, &res, []int{})
-	return res
-}
-
-func pathSumHelper(node *TreeNode, targetSum int, res *[][]int, set []int) {
-	if node == nil {
-		return
-	} else if targetSum-node.Val == 0 && node.Left == nil && node.Right == nil {
-		temp := make([]int, len(set)+1)
-		copy(temp, append(set, node.Val))
-		*res = append(*res, temp)
-	} else {
-		pathSumHelper(node.Left, targetSum-node.Val, res, append(set, node.Val))
-		pathSumHelper(node.Right, targetSum-node.Val, res, append(set, node.Val))
+	var pathSumHelper func(node *TreeNode, targetSum int, set []int)
+	pathSumHelper = func(node *TreeNode, targetSum int, set []int) {
+		if node == nil {
+			return
+		} else if targetSum-node.Val == 0 && node.Left == nil && node.Right == nil {
+			temp := make([]int, len(set)+1)
+			copy(temp, append(set, node.Val))
+			res = append(res, temp)
+		} else {
+			pathSumHelper(node.Left, targetSum-node.Val, append(set, node.Val))
+			pathSumHelper(node.Right, targetSum-node.Val, append(set, node.Val))
+		}
 	}
+	pathSumHelper(root, targetSum, []int{})
+	return res
 }
 
 // @lc code=end
